2f49e3a11062f9b214805277fd5eb02ae6f8911d
Justas Sperauskas <justas@justas.eu>
2020-04-22 23:40:39 +0300

    Test for aged brie

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -21,4 +21,11 @@ class GildedRoseTest {
         assertEquals(1, app.itemQualityAfterDay(agedBrieItem));
     }
 
+    @Test
+    void aged_brie_quality_should_increase_twice_after_sell_in_0() {
+        Item agedBrieItem = new Item(GildedRose.AGED_BRIE, 0, 0);
+        GildedRose app = new GildedRose();
+        assertEquals(2, app.itemQualityAfterDay(agedBrieItem));
+    }
+
 }
