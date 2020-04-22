8592f8ceb674cf43afdffa075c488afdf8d2db15
Justas Sperauskas <justas@justas.eu>
2020-04-22 23:26:34 +0300

    Change sellIn in different method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,75 +2,84 @@ package com.gildedrose;
 
 class GildedRose {
 
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            int newQuality = itemQualityAfterDay(items[i]);
-            items[i].quality = newQuality;
-        }
-    }
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
 
-    private int itemQualityAfterDay(final Item item) {
+			int newQuality = itemQualityAfterDay(items[i]);
+			items[i].quality = newQuality;
 
-        int quality = item.quality;
+			int newSellIn = itemSellInValueAfterDay(items[i]);
+			items[i].sellIn = newSellIn;
+		}
+	}
 
-        if (!item.name.equals(AGED_BRIE)
-            && !item.name.equals(BACKSTAGE_PASSES)) {
-            if (quality > 0) {
-                if (!item.name.equals(SULFURAS)) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1;
+	private int itemQualityAfterDay(final Item item) {
 
-                if (item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
+		int quality = item.quality;
 
-                    if (item.sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
-        }
+		if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+			if (quality > 0) {
+				if (!item.name.equals(SULFURAS)) {
+					quality = quality - 1;
+				}
+			}
+		} else {
+			if (quality < 50) {
+				quality = quality + 1;
 
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
-        }
+				if (item.name.equals(BACKSTAGE_PASSES)) {
+					if (item.sellIn < 11) {
+						if (quality < 50) {
+							quality = quality + 1;
+						}
+					}
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES)) {
-                    if (quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = quality - quality;
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-        }
-      return quality;
-    }
+					if (item.sellIn < 6) {
+						if (quality < 50) {
+							quality = quality + 1;
+						}
+					}
+				}
+			}
+		}
+
+		if (item.sellIn <= 0) {
+			if (!item.name.equals(AGED_BRIE)) {
+				if (!item.name.equals(BACKSTAGE_PASSES)) {
+					if (quality > 0) {
+						if (!item.name.equals(SULFURAS)) {
+							quality = quality - 1;
+						}
+					}
+				} else {
+					quality = quality - quality;
+				}
+			} else {
+				if (quality < 50) {
+					quality = quality + 1;
+				}
+			}
+		}
+		return quality;
+	}
+
+	private int itemSellInValueAfterDay(final Item item) {
+
+	    int sellIn = item.sellIn;
+
+		if (!item.name.equals(SULFURAS)) {
+            sellIn = sellIn - 1;
+		}
+		return sellIn;
+	}
 }
