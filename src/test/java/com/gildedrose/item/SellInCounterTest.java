9967ffbc4951f2f0c857b00e3bb88ed16e625f5f
Justas Sperauskas <justas@justas.eu>
2020-04-25 20:41:37 +0300

    Sell In tests

---
+++ src/test/java/com/gildedrose/item/SellInCounterTest.java
@@ -0,0 +1,28 @@
+package com.gildedrose.item;
+
+import com.gildedrose.GildedRose;
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SellInCounterTest {
+
+	@Test
+	public void aged_brie_sell_in_decreases() {
+		Item agedBrieItem = new Item(GildedRose.AGED_BRIE, 1, 0);
+		assertEquals(0, SellInCounter.itemSellInValueAfterDay(agedBrieItem));
+	}
+
+	@Test
+	public void backstage_passes_sell_in_decreases() {
+		Item backstagePassesItem = new Item(GildedRose.BACKSTAGE_PASSES, 1, 0);
+		assertEquals(0, SellInCounter.itemSellInValueAfterDay(backstagePassesItem));
+	}
+
+	@Test
+	public void sulfuras_sell_in_constant() {
+		Item sulfuras = new Item(GildedRose.SULFURAS, 1, 0);
+		assertEquals(1, SellInCounter.itemSellInValueAfterDay(sulfuras));
+	}
+}
